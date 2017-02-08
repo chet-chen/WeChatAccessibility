e8b78925f73c138d0059ccf421bf15fd27b42091
agentchen <agentchen97@gmail.com>
2017-02-08 13:41:00 +0800

    优化，新增延时设置

--- app/src/main/java/com/demo/wechataccessibility/MainActivity.java
+++ app/src/main/java/com/demo/wechataccessibility/MainActivity.java
@@ -1,23 +1,40 @@
 package com.demo.wechataccessibility;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edit_text);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String s = editText.getText().toString();
+                if (!s.isEmpty()) {
+                    long timeOut = Long.valueOf(s);
+                    saveTimeOut(timeOut);
+                }
                 Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                 startActivity(intent);
             }
         });
+    }
+
+    private void saveTimeOut(final long timeOut) {
+        if (timeOut > 0) {
+            SharedPreferences.Editor editor = getSharedPreferences("timeOut", MODE_PRIVATE).edit();
+            editor.putLong("timeOut", timeOut);
+            editor.apply();
+        }
     }
 }
--- app/src/main/java/com/demo/wechataccessibility/MyAccessibilityService.java
+++ app/src/main/java/com/demo/wechataccessibility/MyAccessibilityService.java
@@ -5,6 +5,7 @@ import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.PowerManager;
 import android.util.Log;
@@ -21,17 +22,17 @@ import java.util.List;
 
 public class MyAccessibilityService extends AccessibilityService {
     private static final String TAG = "MyAccessibilityService";
-    private static final String OPEN_BUTTON_ID = "com.tencent.mm:id/bg7";
-    private static final String BACK_BUTTON_ID = "com.tencent.mm:id/gd";
+    private static final String OPEN_BUTTON_ID = "com.tencent.mm:id/bi3";
+    private static final String BACK_BUTTON_ID = "com.tencent.mm:id/gv";
     private static final String SLOW_TEXT_ID = "com.tencent.mm:id/bg6";
     private boolean isAccessibility = false;
     private boolean isMeUnLock = false;
-    private boolean beforeLauncherUI = false;
     private PowerManager pm;
     private PowerManager.WakeLock wakeLock;
     private KeyguardManager km;
     private KeyguardManager.KeyguardLock keyguardLock;
     private List<AccessibilityNodeInfo> parents;
+    private long timeOut;
 
     @Override
     protected void onServiceConnected() {
@@ -41,14 +42,15 @@ public class MyAccessibilityService extends AccessibilityService {
         km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "bright");
         keyguardLock = km.newKeyguardLock("");
-        Log.d(TAG, "onServiceConnected: ");
+        timeOut = getTimeOut();
+        Log.d(TAG, "onServiceConnected:  timeOut: " + timeOut);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
         switch (eventType) {
-//            通知栏发生变化
+//          通知栏发生变化
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
                 List<CharSequence> texts = event.getText();
                 if (!texts.isEmpty()) {
@@ -65,18 +67,19 @@ public class MyAccessibilityService extends AccessibilityService {
                                     Log.d(TAG, "onAccessibilityEvent: start Accessibility");
                                     autoUnlock();
                                     pendingIntent.send();
-//                                  如果开启抢红包之前就处在LauncherUI则手动getLastPacket();
-                                    if (beforeLauncherUI) {
-                                        try {
-                                            Thread.sleep(500);
-                                            getLastPacket();
-                                        } catch (InterruptedException e) {
-                                            e.printStackTrace();
-                                        }
+//                                  如果开启抢红包之前就处在LauncherUI则不会回调WindowChange,
+//                                  2秒后手动getLastPacket();如果之前不处在LauncherUI，
+//                                  pendingIntent.send()之后会回调onAccessibilityEvent，
+//                                  2秒sleep之后就执行不到。（why？）
+                                    try {
+                                        Thread.sleep(2000);
+                                        Log.d(TAG, "onAccessibilityEvent: 手动getLastPacket()");
+                                        getLastPacket();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
                                     }
                                 } catch (PendingIntent.CanceledException e) {
                                     e.printStackTrace();
-                                    isAccessibility = false;
                                 }
                             }
                         }
@@ -85,30 +88,39 @@ public class MyAccessibilityService extends AccessibilityService {
                 break;
 //            窗口状态发生变化
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                Log.d(TAG, "onAccessibilityEvent: eventContent " + event.toString());
-//                如果抢红包之前就处于launchUI，则不会触发WindowChange事件，在这记录抢红包之前最后一次是否处在launchUI
-                beforeLauncherUI = event.getClassName().toString().equals("com.tencent.mm.ui.LauncherUI");
+                Log.d(TAG, "onAccessibilityEvent: TYPE_WINDOWS_CHANGED" + event.getClassName().toString());
                 if (!isAccessibility) {
                     return;
                 }
                 String className = event.getClassName().toString();
-                Log.d(TAG, "onAccessibilityEvent: Window: " + className);
-                if (className.equals("com.tencent.mm.ui.LauncherUI")) {
-                    getLastPacket();
-                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
-                    inputClick(OPEN_BUTTON_ID);
-                    Log.d(TAG, "onAccessibilityEvent: 开红包");
-                    slow(SLOW_TEXT_ID);//手慢了
-                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")) {
-                    inputClick(BACK_BUTTON_ID);
-                    Log.d(TAG, "onAccessibilityEvent: 退出红包");
-                    isAccessibility = false;
-                    doBack(800);
-                    autoLock();
-                    Log.d(TAG, "onAccessibilityEvent: stop Accessibility");
+                switch (className) {
+                    case "com.tencent.mm.ui.LauncherUI":
+                        Log.d(TAG, "自动 getLastPacket()");
+                        getLastPacket();
+                        break;
+                    case "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI":
+                        inputClick(OPEN_BUTTON_ID);
+                        Log.d(TAG, "onAccessibilityEvent: 开红包");
+                        slow(SLOW_TEXT_ID);//手慢了
+                        break;
+                    case "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI":
+                        inputClick(BACK_BUTTON_ID);
+                        Log.d(TAG, "onAccessibilityEvent: 退出红包");
+                        isAccessibility = false;
+                        doBack();
+                        autoLock();
+                        Log.d(TAG, "onAccessibilityEvent: stop Accessibility");
+                        break;
+                    default:
+                        break;
                 }
                 break;
         }
+    }
+
+    @Override
+    public void onInterrupt() {
+
     }
 
     private void inputClick(String clickId) {
@@ -124,16 +136,21 @@ public class MyAccessibilityService extends AccessibilityService {
 
     public void getLastPacket() {
 //        点击最后一个红包
+        try {
+            Thread.sleep(timeOut);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Log.d(TAG, "getLastPacket: 进入微信");
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
         parents.clear();
         recycle(rootNode);
         if (parents.size() > 0) {
-            Log.d(TAG, "getLastPacket: click " + parents.get(parents.size() - 1).performAction(AccessibilityNodeInfo.ACTION_CLICK));
+            parents.get(parents.size() - 1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
             Log.d(TAG, "onAccessibilityEvent: 点击最后一个红包");
         } else {
             Log.d(TAG, "getLastPacket: 没有红包");
-            doBack(500);
+            doBack();
             isAccessibility = false;
             autoLock();
         }
@@ -145,7 +162,7 @@ public class MyAccessibilityService extends AccessibilityService {
             if (rootNode.getText() != null) {
                 if ("领取红包".equals(rootNode.getText().toString())) {
                     if (rootNode.isClickable()) {
-                        Log.d(TAG, "recycle: click " + rootNode.performAction(AccessibilityNodeInfo.ACTION_CLICK));
+                        rootNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                     }
                     AccessibilityNodeInfo parent = rootNode.getParent();
                     while (parent != null) {
@@ -176,11 +193,11 @@ public class MyAccessibilityService extends AccessibilityService {
                 String s = accessibilityNodeInfo.getText().toString();
                 if (s.contains("手慢了")) {
                     Log.d(TAG, "slow: 手慢了");
-                    doBack(0);
-                    doBack(800);
+                    doBack();
+                    doBack();
                     autoLock();
-                    Log.d(TAG, "slow: stop Accessibilty");
                     isAccessibility = false;
+                    Log.d(TAG, "slow: stop Accessibilty");
                 }
             }
         }
@@ -195,19 +212,6 @@ public class MyAccessibilityService extends AccessibilityService {
             if (km.isDeviceLocked()) {
                 Log.d(TAG, "autoUnlock: sdk >= 22: 屏幕被密码锁柱");
                 wakeLock.release();
-                beforeLauncherUI = false;
-            } else {
-                if (km.inKeyguardRestrictedInputMode()) {
-                    keyguardLock.disableKeyguard();
-                    Log.d(TAG, "onAccessibilityEvent: 尝试解锁");
-                    isMeUnLock = true;
-                }
-            }
-        } else {
-            if (km.isKeyguardLocked()) {
-                Log.d(TAG, "autoUnlock: sdk < 22 屏幕被密码锁柱");
-                wakeLock.release();
-                beforeLauncherUI = false;
             } else {
                 if (km.inKeyguardRestrictedInputMode()) {
                     keyguardLock.disableKeyguard();
@@ -220,6 +224,11 @@ public class MyAccessibilityService extends AccessibilityService {
 
     private void autoLock() {
         if (isMeUnLock) {
+            try {
+                Thread.sleep(timeOut);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             keyguardLock.reenableKeyguard();
             Log.d(TAG, "autoLock: 自动锁");
             if (wakeLock != null && pm.isScreenOn()) {
@@ -227,17 +236,12 @@ public class MyAccessibilityService extends AccessibilityService {
                 Log.d(TAG, "autoLock: 自动灭");
             }
             isMeUnLock = false;
-            beforeLauncherUI = false;
         }
     }
 
-    private void doBack(int time) {
-        if (time == 0) {
-            this.performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
-            return;
-        }
+    private void doBack() {
         try {
-            Thread.sleep(time);
+            Thread.sleep(timeOut);
             this.performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -245,8 +249,9 @@ public class MyAccessibilityService extends AccessibilityService {
         }
     }
 
-    @Override
-    public void onInterrupt() {
-        Log.d(TAG, "onInterrupt: ");
+    private long getTimeOut() {
+        SharedPreferences sp = getSharedPreferences("timeOut", MODE_PRIVATE);
+        long timeOUt = sp.getLong("timeOut", 0);
+        return timeOUt > 0 ? timeOUt : 500;
     }
 }
